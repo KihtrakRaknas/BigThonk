4b755e4c7c55bce69044e875f2bc7fc0f61015ce
KihtrakRaknas <karthikandsankar@gmail.com>
2020-04-24 05:09:15 -0400

    Updates

---
+++ precache-manifest.7df427aef4541c146182f193b15f6e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75557cad4a8a40102c9ed32094a2324",
+    "url": "/BigThonk/index.html"
+  },
+  {
+    "revision": "05aa21775b20ab22ae65",
+    "url": "/BigThonk/static/css/main.cb5de4a9.chunk.css"
+  },
+  {
+    "revision": "323b005f0907ff1c0606",
+    "url": "/BigThonk/static/js/2.64c4fa01.chunk.js"
+  },
+  {
+    "revision": "78055f8dda8e9411d36c39a050b7f55e",
+    "url": "/BigThonk/static/js/2.64c4fa01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05aa21775b20ab22ae65",
+    "url": "/BigThonk/static/js/main.cde322c1.chunk.js"
+  },
+  {
+    "revision": "ec393c6caac7fb562213",
+    "url": "/BigThonk/static/js/runtime-main.b7f68a12.js"
+  }
+]);
